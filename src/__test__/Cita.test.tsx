a9415a02e40f5e15a751ff93b867763307ee17b8
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2022-12-05 10:16:39 -0300

    customRender in tests

--- src/__test__/Cita.test.tsx
+++ src/__test__/Cita.test.tsx
@@ -1,40 +1,23 @@
 import { render, act, fireEvent, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Cita from '../features/quote/Cita'
-import { Provider } from "react-redux";
-import { store } from '../app/store'
 import userEvent from "@testing-library/user-event";
+import { customRender } from '../test-utils'
 
 describe("Pruebas en <Cita />", () => {
 
-    test("Renderizado inicial", () => {
-        let component = render(
-            <Provider store={store}>
-                <Cita />
-            </Provider>
-        );
-
-        expect(component.container).toBeInTheDocument();
-    });
-
+    let renderComponent = () =>  customRender(<Cita />)
 
     test("Renderizado inicial sin cita", async () => {
-        render(
-            <Provider store={store}>
-                <Cita />
-            </Provider>
-        );
+        renderComponent()
+
         expect(screen.getByText('No se encontro ninguna cita')).toBeInTheDocument();
     });
 
 
     test("Mostrar mensaje de cargando", async () => {
-        render(
-            <Provider store={store}>
-                <Cita />
-            </Provider>
-        );
-        // const onChange = jest.fn()
+        renderComponent()
+
         const btnAleatorio = screen.getByText("Obtener cita aleatoria")
         expect(btnAleatorio).toBeEnabled();
 
@@ -43,12 +26,8 @@ describe("Pruebas en <Cita />", () => {
     });
 
 
-    test("Mostrar nombre del personaje ingresado", async () => {
-        render(
-            <Provider store={store}>
-                <Cita />
-            </Provider>
-        );
+    test("Mostrar cita y nombre del personaje ingresado", async () => {
+        renderComponent()
 
         const input = screen.getByPlaceholderText('Ingresa el nombre del autor')
 
@@ -56,24 +35,20 @@ describe("Pruebas en <Cita />", () => {
 
         userEvent.type(input, "Lisa")
 
-        await waitFor(() => expect(screen.findByText("Lisa Simpson'")))
+        await waitFor(() => expect(screen.findByText("Lisa Simpson")))
         await waitFor(() => expect(screen.findByText("Shut up, brain. I got friends now. I don't need you anymore.")))
     });
 
 
     test("Mensaje error al ingresar números", async () => {
-        render(
-            <Provider store={store}>
-                <Cita />
-            </Provider>
-        );
+        renderComponent()
 
         const input = await screen.findByPlaceholderText('Ingresa el nombre del autor')
         const btnCita = screen.getByTestId("quote-button")
 
-        userEvent.type(input, "lisa")
+        userEvent.type(input, "1233")
         userEvent.click(btnCita)
-
+        
         await waitFor(() => {
             expect(screen.getByText('Por favor ingrese un nombre válido')).toBeInTheDocument();
         })
@@ -81,14 +56,9 @@ describe("Pruebas en <Cita />", () => {
 
 
     test("Correcto funcionamiento del botón 'Borrar' ", async () => {
-        render(
-            <Provider store={store}>
-                <Cita />
-            </Provider>
-        );
+        renderComponent()
 
         const input = screen.getByPlaceholderText('Ingresa el nombre del autor');
-        // const btnBorrar = screen.getByText("Borrar");
         const btnCita = screen.getByTestId("quote-button")
         const btnBorrar = screen.getByTestId("clean-button")
 
--- src/__test__/mocks/handlers.ts
+++ src/__test__/mocks/handlers.ts
@@ -1,21 +1,29 @@
-import { rest } from 'msw'
 
-const data = {
-	quote: "Shut up, brain. I got friends now. I don't need you anymore.",
-	character: 'Lisa Simpson',
-	image: 'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083',
-	characterDirection: 'Right'
-}
+import { rest } from "msw";
+import { API_URL } from "../../app/constants";
+import { ICita } from "../../features/quote/types";
 
-const handlers = [
-	rest.get('https://thesimpsonsquoteapi.glitch.me/quotes', (req, res, ctx) => {
-		return res(
-			ctx.status(200),
-			ctx.json({
-				results: data
-			})
-		);
+export const citaPorBusqueda: ICita = {
+	cita: "Shut up, brain. I got friends now. I don't need you anymore.",
+	personaje: 'Lisa Simpson',
+	imagen: 'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083',
+	direccionPersonaje: 'Right'
+};
+
+export const citaAleatoria: ICita = {
+	cita: "I'm sleeping in the bath tub.",
+	personaje: "Marge Simpson",
+	imagen:
+		"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FMargeSimpson.png?1497567512205",
+	direccionPersonaje: "Right",
+};
+
+export const handlers = [
+	rest.get(API_URL, (req, res, ctx) => {
+		const citaFinal = req.url.searchParams.get("character")
+			? citaPorBusqueda
+			: citaAleatoria;
+		return res(ctx.status(200), ctx.json([citaFinal]));
 	}),
 ];
 
-export { data, handlers };
